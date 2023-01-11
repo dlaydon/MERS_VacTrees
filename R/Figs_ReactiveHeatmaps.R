2e92dbce4486a2559f2a1e22071e954ae1090de4
dlaydon <d.laydon@imperial.ac.uk>
2023-01-11 16:22:58 +0000

    amended Reactive waning script

--- R/Figs_ReactiveHeatmaps.R
+++ R/Figs_ReactiveHeatmaps.R
@@ -1,7 +1,7 @@
-
-rm(list=ls())
-Sys.time()
-gc()
+# Libraries
+library(dplyr)
+library(tidyr)
+library(viridis)
 
 require(plot3D)
 require(here)
@@ -14,17 +14,12 @@ library(gridExtra)
 library(colorRamps)
 #install.packages("cowplot")
 library("cowplot")
-
-# Libraries
-library(dplyr)
-library(tidyr)
-library(viridis)
+require(stringr)
 
 ParDefaults = par()
 OrigMAR 	= ParDefaults$mar
 
 ############################################################################
-options(width = 108L)
 OrigMAI = par ("mai") ### record if using layout functions
 OrigMAR = par ("mar") ### record if using layout functions
 
@@ -37,109 +32,156 @@ source(file.path(R_ScriptDirectory, "DirectoryFunctions.R"))
 source(file.path(R_ScriptDirectory, "LayoutPlottingFunctions.R"))
 
 # Import summary model runs
-ModelRuns = read.table(file = file.path(CppOutputDirectory, paste0("ModelRunsSummary.txt")), header = T, sep = "\t") 
+ModelRuns = read.table(file = file.path(ProjectDirectory, paste0("ModelRunsSummary.txt")), header = T, sep = "\t")
+ModelRuns = ModelRuns[ModelRuns$DoTriggers == 0, ]
+str(ModelRuns)
+
+ZVar_Chars 	= c("PropCasesAverted"	, "DeathsAverted_CF", "Cases_DirectlyProtected"	, "Cases_IndirectlyProtected", "Deaths_DirectlyProtected", "Deaths_IndirectlyProtected")
+LegendNames	= c("Proportion of\ncases averted", "Proportion of\ndeaths averted", "Proportion of\ncases averted\n(directly)", "Proportion of\ncases averted\n(indirectly)", "Proportion of\ndeaths averted\n(directly)", "Proportion of\ndeaths averted\n(indirectly)")
+
+CamelEffsSubset = unique(ModelRuns$Efficacy_CamelControls)[c(1,4,6)]
+DurationSubset 	= unique(ModelRuns$VaccineDuration)[c(7,6,5,4,2)]
+
+WindowsList = list(c(0, 548), c(0,180), c(181,364), c(365,548))
+
+RW_String 	= "_RW"
+WaningInReactive = 1
 
 XVar_Char = "ImplementationDelay"
 YVar_Char = "Efficacy_Start"
 
-ReactLevel 	= "HOSPITAL"; 
-ZVar_Chars 	= c("PropCasesAverted"	, "DeathsAverted_CF")
-PlotNames 	= c("cases averted"		, "deaths averted"	)
-LegendNames	= c("Proportion of\ncases averted", "Proportion of\ndeaths averted")
-#ZVar_Chars 	= c(ListOfCounterfactuals, "Peak_Reduction")
-#LegendNames	= c("Proportion of\ncases averted"	, "Proportion of\ncases averted\n(healthcare\nworkers only)" , "Proportion of\ncases averted\n(excluding\nhealthcare workers)", 
-#		"Proportion of\ndeaths averted"	, "Proportion of\ndeaths averted\n(healthcare\nworkers only)", "Proportion of\ndeaths averted\n(excluding\nhealthcare workers)", 
-#		"Final\ncase day"				, "Epidemic\npeak size", "Proportional\nreduction in\nepidemic peak")
-Stat 		= "_Mean"
+MinDay_Pruning = Window[1]
+MaxDay_Pruning = Window[2]
+if (MinDay_Pruning > 0 | MaxDay_Pruning < (MAX_ONSET_DAY - 1))	
+	WindowString = paste0("_s", MinDay_Pruning, "_f", MaxDay_Pruning) else	WindowString = "" 
+WindowString = ""
 
-CamelEffsSubset = unique(ModelRuns$Efficacy_CamelControls)[c(1,4,6)]
-
-PlotNum = 1
-for (PlotNum in 1:length(ZVar_Chars))
-{
-	if (ZVar_Chars[PlotNum] == "FinalCaseDate_CF") LIMITS = NULL else LIMITS = c(0,1)
+EffCamelIndex = 1
+for (EffCamelIndex in 1:length(CamelEffsSubset))
+{	 
+	Efficacy_CamelControls = CamelEffsSubset[EffCamelIndex]
+	cat(paste0("Efficacy_CamelControls ", Efficacy_CamelControls, ", \t"))
 	
-	for (Stat in c("_Mean", "_LowerCrI", "_UpperCrI"))
+	ExpWaning = 0
+	for (ExpWaning in 1:0)
 	{
-		for (EffCamelIndex in 1:length(CamelEffsSubset))
-		{	 
-			Efficacy_CamelControls = CamelEffsSubset[EffCamelIndex]
-			for (ReactLevel in unique(ModelRuns$ReactLevel))
+		if (ExpWaning == 0)	HillString 	= "_Hill" else HillString = ""
+		
+		cat(paste0("ExpWaning ", ExpWaning, ", "))
+		
+		PlotNum = 2
+		for (PlotNum in 1:length(ZVar_Chars))
+		{
+			if (ZVar_Chars[PlotNum] == "FinalCaseDate_CF") LIMITS = NULL else LIMITS = c(0,1)
+			
+			cat(paste0(ZVar_Chars[PlotNum], " "))
+			
+			Stat 		= "_Mean"
+			for (Stat in c("_Mean", "_LowerCrI", "_UpperCrI"))
 			{
-				# subset model runs
-				ModelRuns_subset = ModelRuns[ModelRuns$VacCampStrategy == "REACTIVE" & 
-								ModelRuns$ReactLevel == ReactLevel & ModelRuns$Efficacy_CamelControls == Efficacy_CamelControls & 
-								ModelRuns$Efficacy_Start != 0 & 
-								ModelRuns$ImmunityDelay == 14, ]
-				ModelRuns_subset$Efficacy_Start = ModelRuns_subset$Efficacy_Start * 100
-				
-				cat(paste0("ReactLevel ", ReactLevel, ", "))
-				
-				if (ReactLevel == "HOSPITAL")	ReactLevel_String = "Hospital"
-				if (ReactLevel == "REGIONAL")	ReactLevel_String = "Regional"
-				if (ReactLevel == "NATIONAL")	ReactLevel_String = "National"
-				
-				PlotTitle = paste0("\n", ReactLevel_String, " level")
-				if (ReactLevel == "HOSPITAL")	PlotTitle = paste0("Camel control effectiveness = ", Efficacy_CamelControls * 100, "%", PlotTitle)
-				
-				# make heatmap
-				GPlot <- ggplot(ModelRuns_subset, aes_string(x = XVar_Char, y = YVar_Char, fill = paste0(ZVar_Chars[PlotNum], Stat))) + 
-						geom_tile() + 
-						ggtitle(PlotTitle) + 
-						labs(y = "", x = "") + 
-						theme_minimal() + 
-						theme(plot.title = element_text(size = 30), axis.text = element_text(size = 25), 
-								legend.text = element_text(size = 25), legend.title = element_text(size = 31),
-								legend.key.size = unit(2.25, 'cm'), legend.key.width = unit(2.2, 'cm')) + 
-						guides(fill = FALSE) + 
-						scale_fill_gradientn(
-								colors = rev(matlab.like2(50)), # various colour candidates here: https://www.nceas.ucsb.edu/sites/default/files/2020-04/colorPaletteCheatsheet.pdf #colors = heat.colors(50), #colors = hcl.colors(50, palette = "RdYlBu"), palette = "RdYlGn", palette = "YlOrRd"
-								limits = LIMITS)
-				
-				# assign it to nice name
-				assign(paste0("CamelEff_", EffCamelIndex, "_", ReactLevel, "_Plot"), GPlot)
-				
-				if (ReactLevel == "NATIONAL")
+				cat(paste0(Stat, "\n"))
+				Duration = DurationSubset[1]
+				for (DurationIndex in 1:length(DurationSubset))
 				{
-					GPlotWLegend <- GPlot + guides(fill = guide_legend(title = LegendNames[PlotNum]), limits = LIMITS) 
-					legend <- get_legend(GPlotWLegend)
-					assign(paste0("Legend_Row_", EffCamelIndex),  legend)	
+					Duration = DurationSubset[DurationIndex]
+					cat(paste0("Duration ", Duration, ", \t "))
+					
+					ReactLevel 	= "HOSPITAL"; 
+					ReactLevel 	= "REGIONAL"; 
+					ReactLevel 	= "NATIONAL"; 
+					for (ReactLevel in unique(ModelRuns$ReactLevel))
+					{
+						cat(paste0(ReactLevel, ", \t"))
+						
+						ModelRuns_subset = ModelRuns[ModelRuns$VacCampStrategy 	== "REACTIVE" 				& 
+										ModelRuns$ReactLevel 					== ReactLevel 				& 
+										ModelRuns$WaningInReactive				== WaningInReactive			&
+										ModelRuns$Efficacy_CamelControls 		== Efficacy_CamelControls 	& 
+										ModelRuns$Efficacy_Start 				!= 0 						& 
+										ModelRuns$MinDay_Pruning 				== MinDay_Pruning 			& 
+										ModelRuns$MaxDay_Pruning 				== MaxDay_Pruning 			& 
+										ModelRuns$ImmunityDelay 				== 14						, ]
+						
+						if (WaningInReactive == 1)
+							ModelRuns_subset = ModelRuns_subset[ModelRuns_subset$VaccineDuration == Duration 	&
+											ModelRuns_subset$ExpWaning 				== ExpWaning ,]					
+						
+						
+						ReactLevel_String = str_to_sentence(ReactLevel, locale = "en")
+						PlotTitle = paste0("\n", ReactLevel_String, " level")
+						
+						if (ExpWaning == 0			)	DurString 	= "Half-life" 	else DurString = "Mean duration of protection"
+						if (Duration == 1			)	YearOrYears = "year" 		else YearOrYears = "years"
+						if (ReactLevel == "HOSPITAL")	PlotTitle 	= paste0(DurString, " = ", Duration, " ", YearOrYears, PlotTitle)
+						
+						# make heatmap
+						GPlot <- ggplot(ModelRuns_subset, aes_string(x = XVar_Char, y = YVar_Char, fill = paste0(ZVar_Chars[PlotNum], Stat))) + 
+								geom_tile() + 
+								ggtitle(PlotTitle) + 
+								labs(y = "", x = "") + 
+								theme_minimal() + 
+								theme(plot.title = element_text(size = 30), axis.text = element_text(size = 25), 
+										legend.text = element_text(size = 25), legend.title = element_text(size = 31),
+										legend.key.size = unit(2.25, 'cm'), legend.key.width = unit(2.2, 'cm'), 
+										panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + 
+								guides(fill = "none") + 
+								scale_fill_gradientn(colors = rev(matlab.like2(50)), limits = LIMITS, breaks = seq(LIMITS[1], LIMITS[2], by = 0.1)) # various colour candidates here: https://www.nceas.ucsb.edu/sites/default/files/2020-04/colorPaletteCheatsheet.pdf #colors = heat.colors(50), #colors = hcl.colors(50, palette = "RdYlBu"), palette = "RdYlGn", palette = "YlOrRd"
+						
+						# assign it to nice name
+						assign(paste0("Dur_", Duration, "_", ReactLevel, "_Plot"), GPlot)
+						
+						if (ReactLevel == "NATIONAL")
+						{
+							GPlotWLegend <- GPlot + guides(fill = guide_legend(title = LegendNames[PlotNum]), limits = LIMITS, breaks = seq(LIMITS[1], LIMITS[2], by = 0.1)) 
+							legend <- get_legend(GPlotWLegend)
+							assign(paste0("Legend_Row_", EffCamelIndex),  legend)	
+						}
+					}
+					cat(paste0("\n"))
 				}
+				
+				Filename = paste0("ReactiveW_", ZVar_Chars[PlotNum], WindowString, HillString, "CamelEff_", Efficacy_CamelControls, Stat)
+				png(filename 	= file.path(here("Plots/"), paste0(Filename, ".png")),	res = 500, units = "in"	, width = 21, height = 27.5)
+				#pdf(file 		= file.path(here("Plots/"), paste0(Filename, ".pdf"))							, width = 21, height = 17)
+				
+				LayoutMatrix = matrix(1:(length(DurationSubset) * length(unique(ModelRuns$ReactLevel))), nrow = length(DurationSubset), byrow = T)
+				LayoutMatrix = cbind(LayoutMatrix, max(LayoutMatrix) + 1)
+				
+				grid.arrange(
+						
+						Dur_1_HOSPITAL_Plot	, 
+						Dur_1_REGIONAL_Plot	, 
+						Dur_1_NATIONAL_Plot	, 
+						
+						Dur_2_HOSPITAL_Plot	, 
+						Dur_2_REGIONAL_Plot	, 
+						Dur_2_NATIONAL_Plot	, 
+						
+						Dur_5_HOSPITAL_Plot	, 
+						Dur_5_REGIONAL_Plot	, 
+						Dur_5_NATIONAL_Plot	, 
+						
+						Dur_10_HOSPITAL_Plot	, 
+						Dur_10_REGIONAL_Plot	, 
+						Dur_10_NATIONAL_Plot	, 
+						
+						Dur_20_HOSPITAL_Plot	, 
+						Dur_20_REGIONAL_Plot	, 
+						Dur_20_NATIONAL_Plot	, 
+						Legend_Row_1			,
+						
+						layout_matrix = LayoutMatrix,
+						widths 	= c(1,1,1,0.8)			,
+						
+						nrow 	= length(DurationSubset), 
+						bottom 	= textGrob("Reaction time (days)"	, gp = gpar(fontsize = 35, font = 1), hjust = 0.875), 
+						left 	= textGrob("Vaccine efficacy (%)"	, gp = gpar(fontsize = 35, font = 1), rot = 90)) ## would be better to use grid arrange with characters but leave for now.
+				dev.off()
 			}
-			cat(paste0(Stat, ", "))
 		}
-		
-		png(filename = file.path(here("Plots/"), paste0("Heatmap_Reactive", Stat, "_", ZVar_Chars[PlotNum], ".png")), 
-				res = 500, units = "in", width = 21, height = 17)
-		
-		LayoutMatrix = matrix(1:(length(CamelEffsSubset) * length(unique(ModelRuns$ReactLevel))), 
-				nrow = length(CamelEffsSubset), byrow = T)
-		LayoutMatrix = cbind(LayoutMatrix, max(LayoutMatrix) + 1)
-		
-		grid.arrange(
-				
-				CamelEff_1_HOSPITAL_Plot	, 
-				CamelEff_1_REGIONAL_Plot	, 
-				CamelEff_1_NATIONAL_Plot	, 
-				CamelEff_2_HOSPITAL_Plot	, 
-				CamelEff_2_REGIONAL_Plot	, 
-				CamelEff_2_NATIONAL_Plot	, 
-				CamelEff_3_HOSPITAL_Plot	, 
-				CamelEff_3_REGIONAL_Plot	, 
-				CamelEff_3_NATIONAL_Plot	, 
-				Legend_Row_1					,
-				
-				layout_matrix = LayoutMatrix,
-				widths 	= c(1,1,1,0.8)			,
-				
-				nrow 	= length(CamelEffsSubset), 
-				bottom 	= textGrob("React time (days)"		, gp = gpar(fontsize = 35, font = 1), hjust = 0.97), 
-				left 	= textGrob("Vaccine efficacy (%)"	, gp = gpar(fontsize = 35, font = 1), rot = 90)) ## would be better to use grid arrange with characters but leave for now.
-		dev.off()
-		
-		cat(paste0("\n"))
-	}
+	}	
 }
-class(GPlot)
 
-CloseOpenPlotDevices()	
+CloseOpenPlotDevices()
+warnings()
+
